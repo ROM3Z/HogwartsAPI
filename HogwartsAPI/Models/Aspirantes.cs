7d24b703417dc2757c76a110563fe6d40e244ed1
Isaac Rodriguez <isaac.rod33@gmail.com>
2021-11-03 11:30:14 -0400

    Modificaciones

--- HogwartsAPI/Models/Aspirantes.cs
+++ HogwartsAPI/Models/Aspirantes.cs
@@ -11,7 +11,6 @@ namespace HogwartsAPI.Models
 
         public int Id { get; set; }
 
-      
         [Required]
         [StringLength(20, MinimumLength = 3, ErrorMessage ="Su Nombre debe tener un minimo de 3 caracteres y un maximo de 20")]
         public string Nombre { get; set; }
@@ -20,15 +19,15 @@ namespace HogwartsAPI.Models
         [StringLength(20, MinimumLength = 3, ErrorMessage = "Su Apellido debe tener un minimo de 3 caracteres y un maximo de 20")]
         public string Apellido { get; set; }
 
-       
+        [Range(1, 100000000000)]
         public int Identificacion { get; set; }
 
-   
-
+        [Range(1, 99)]
         public int Edad { get; set; }
 
  
         public string IdCasa { get; set; }
+      
     }
 
   
